bb740f57d6fa03c3054f955d23632b3951fde19f
caseyv22 <caseyvillanueva@gmail.com>
2026-04-24 18:12:48 -0700

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,13 +7,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        'st-green': '#085041',
+        'st-green': '#064029',
         'st-accent': '#1D9E75',
         'st-light': '#E1F5EE',
+        'st-offwhite': '#FAFBFF',
+        'st-cloud': '#EDEFF7',
+        'st-smoke': '#D3D6E0',
+        'st-graphite': '#6E7180',
+        'st-arsenic': '#40424D',
+        'st-phantom': '#1E1E24',
       },
       fontFamily: {
-        display: ['"Bebas Neue"', 'sans-serif'],
-        body: ['"Inter"', 'sans-serif'],
+        display: ['"Manrope"', 'sans-serif'],
+        body: ['"Manrope"', 'sans-serif'],
       },
     },
   },
